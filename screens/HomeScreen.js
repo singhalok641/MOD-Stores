ebf5344a52122c4a3d33576cd9d69af4d85af5f2
Alok Singh <singhalok641@gmail.com>
2017-11-28 17:19:08 +0530

    added past orders screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -7,182 +7,127 @@ import {
   Text,
   TouchableOpacity,
   View,
+  ActivityIndicator,
+  AsyncStorage
 } from 'react-native';
+import { 
+  Container, 
+  Header, 
+  Content, 
+  List, 
+  ListItem, 
+  Thumbnail, 
+  Body, 
+  Left, 
+  Right, 
+  Badge, 
+  Picker, 
+  Form, 
+  Item as FormItem } from 'native-base';
+
 import { WebBrowser } from 'expo';
 
 import { MonoText } from '../components/StyledText';
 
 export default class HomeScreen extends React.Component {
   static navigationOptions = {
-    header: null,
+    title:'Past Orders',
   };
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-          <View style={styles.welcomeContainer}>
-            <Image
-              source={
-                __DEV__
-                  ? require('../assets/images/robot-dev.png')
-                  : require('../assets/images/robot-prod.png')
-              }
-              style={styles.welcomeImage}
-            />
-          </View>
-
-          <View style={styles.getStartedContainer}>
-            {this._maybeRenderDevelopmentModeWarning()}
-
-            <Text style={styles.getStartedText}>Get started by opening</Text>
-
-            <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-              <MonoText style={styles.codeHighlightText}>screens/HomeScreen.js</MonoText>
-            </View>
-
-            <Text style={styles.getStartedText}>
-              Change this text and your app will automatically reload.
-            </Text>
-          </View>
-
-          <View style={styles.helpContainer}>
-            <TouchableOpacity onPress={this._handleHelpPress} style={styles.helpLink}>
-              <Text style={styles.helpLinkText}>Help, it didn’t automatically reload!</Text>
-            </TouchableOpacity>
-          </View>
-        </ScrollView>
-
-        <View style={styles.tabBarInfoContainer}>
-          <Text style={styles.tabBarInfoText}>This is a tab bar. You can edit it in:</Text>
-
-          <View style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-            <MonoText style={styles.codeHighlightText}>navigation/MainTabNavigator.js</MonoText>
-          </View>
-        </View>
-      </View>
-    );
-  }
-
-  _maybeRenderDevelopmentModeWarning() {
-    if (__DEV__) {
-      const learnMoreButton = (
-        <Text onPress={this._handleLearnMorePress} style={styles.helpLinkText}>
-          Learn more
-        </Text>
-      );
-
-      return (
-        <Text style={styles.developmentModeText}>
-          Development mode is enabled, your app will be slower but you can use useful development
-          tools. {learnMoreButton}
-        </Text>
-      );
-    } else {
-      return (
-        <Text style={styles.developmentModeText}>
-          You are not in development mode, your app will run at full speed.
-        </Text>
-      );
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+      orders:{},
     }
   }
 
-  _handleLearnMorePress = () => {
-    WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/guides/development-mode');
-  };
+  render() {
 
-  _handleHelpPress = () => {
-    WebBrowser.openBrowserAsync(
-      'https://docs.expo.io/versions/latest/guides/up-and-running.html#can-t-see-your-changes'
+   /* if (this.state.isLoading) {
+      return (
+        <View style={{flex: 1, paddingTop: 20}}>
+          <ActivityIndicator />
+        </View>
+      );
+    }*/
+
+    const { navigate } = this.props.navigation;
+    var orders =   [{"id":"UB00201BC22","amount":"50","status":"Delivered","date":"21 Sep 2017","time":"14:24"},
+                    {"id":"UB00201BC30","amount":"100","status":"Cancelled","date":"21 Sep 2017","time":"14:24"},
+                    {"id":"UB00201BC36","amount":"320","status":"Delivered","date":"21 Sep 2017","time":"14:24"}];
+    return (
+      <Container style={styles.container}>
+        <Content>
+          <List dataArray={orders}
+            renderRow={(order) =>
+              <ListItem>
+              <View style={styles.view}>
+                  <View style={styles.innerviewleft}>
+                    <Text style={styles.boldtext}>{order.id}</Text>
+                    <Text>{order.time}</Text>
+                  </View>
+
+                  <View style={styles.innerviewright}>
+                    <Text style={styles.boldtext}>₹{order.amount}</Text>
+                    {
+                          order.status == 'Delivered' ? (
+                            <Badge style={{ backgroundColor: '#388e3c',width:80,justifyContent: 'center'}}>
+                              <Text style={{ color: 'white',textAlign: 'center'}}>{order.status}</Text>
+                            </Badge>
+                          )
+                          :
+                          (
+                            <Badge style={{ backgroundColor: '#f39c12',width:80,justifyContent: 'center'}}>
+                              <Text style={{ color: 'white',textAlign: 'center'}}>{order.status}</Text>
+                            </Badge>
+                          )
+                    }
+                  </View>
+                </View>
+              </ListItem>
+            }>
+          </List>
+        </Content>
+      </Container>
     );
-  };
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: 15,
     backgroundColor: '#fff',
   },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
+  boldcolortext:{
+    fontWeight:'bold',
+    color: '#3498db'
   },
-  contentContainer: {
-    paddingTop: 30,
+  boldtext:{
+    fontWeight:'bold',
   },
-  welcomeContainer: {
-    alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
+  lefttext:{
+    fontWeight:'bold',
+    fontSize: 30,
+    color: '#2980b9'
   },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
+  view: {
+    flex:1,
+    flexDirection:'row',
+    justifyContent: 'space-between',
+    alignItems: 'stretch' 
   },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
+  innerviewleft: {
+    flexDirection:'column',
+    justifyContent: 'space-around',
   },
-  homeScreenFilename: {
-    marginVertical: 7,
+  innerviewright: {
+    flexDirection:'column',
+    justifyContent: 'space-around',
+    alignItems: 'center', 
   },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
-  },
+  boldtext:{
+    fontWeight:'bold',
+  }
 });
